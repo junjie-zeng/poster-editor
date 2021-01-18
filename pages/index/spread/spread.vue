c32fb46721943cd63cd73370202b893b50a22b6b
junjiezeng <junjie.zeng@focussend.com>
2021-01-18 16:20:18 +0800

    banana

--- pages/index/spread/spread.vue
+++ pages/index/spread/spread.vue
@@ -4,44 +4,32 @@
 		<view class="ps-body">
 			<view class="ps-inner-box ps-bg-linear">
 				<view class="ps-full-box">
-					<swiper class="swiper" >
-						<swiper-item>
-							<image src="../../../static/image/1.png" ></image>
-						</swiper-item>
-						<swiper-item>
-							<image src="../../../static/image/2.png" ></image>
-						</swiper-item>
-						<swiper-item>
-							<image src="../../../static/image/3.png" ></image>
+					<swiper class="swiper" autoplay interval="1500" circular>
+						<swiper-item v-for="(item,index) in banans" :key = "index">
+							<image :src="item" ></image>
 						</swiper-item>
 					</swiper>
 				</view>
 				<view class="ps-full-box">
 					<view class="ps-scroll">
 						<view class="scroll-left">
-							<swiper class="swiper" vertical autoplay  interval="1500" circular style="height: 90rpx;" >
-								<swiper-item>
+							<swiper class="swiper" vertical autoplay  interval="2000" circular style="height: 90rpx;" >
+								<swiper-item v-for="(item,index) in pageList" :key = "index">
 									<view class="scroll-left-item">
-										<view class="zfx"></view>
-										<view class="tip">青藏高原</view>
-									</view>
-								</swiper-item>
-								<swiper-item>
-									<view class="scroll-left-item">
-										<view class="zfx"></view>
-										<view class="tip">adfasd</view>
+										<view class="zfx iconfont icon-integral-fill">
+											<!-- <view class="iconfont icon-3jichuyuansu"></view> -->
+										</view>
+										<view class="tip">{{item.name}}</view>
 									</view>
 								</swiper-item>
 							</swiper>
 						</view>
-						              
 						<view class="scroll-right">
 							<navigator url="/pages/poster/more-poster/index">
 								<view class="many">更多</view>
 								<view class="iconfont icon-arrow-right"></view>
 							</navigator>
 						</view>
-						
 					</view>
 				</view>
 			</view>
@@ -56,7 +44,7 @@
 							<view class="ps-td">
 								<view class="poster-recommend">
 									<view class="poster-item" v-for="(item,index) in pageList" :key = "item.id">
-										<navigator :url="'/pages/poster/edit-poster/index?id='+item.id">
+										<navigator :url="`/pages/poster/edit-poster/index?id=${item.id}&type=new`">
 											<view class="ps-cover">
 												<image :src="item.url" ></image>
 												<view class="poster-tips">
@@ -83,14 +71,19 @@
 	export default {
 		data() {
 			return {
-				
+				banans:[
+					'../../../static/image/banans/1.jpg',
+					'../../../static/image/banans/2.jpg',
+					'../../../static/image/banans/3.jpg',
+					'../../../static/image/banans/4.jpg',
+				]
 			}
 		},
 		computed:{
 			...mapState(['pageList'])
 		},
 		mounted() {
-			console.log(this.pageList)
+			// console.log(this.pageList)
 		},
 		methods: {
 			
