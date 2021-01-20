13f4ed5c4760ad8b665570cb9de274102509ce15
junjiezeng <junjie.zeng@focussend.com>
2021-01-20 17:47:47 +0800

    tools

--- common/tools/index.js
+++ common/tools/index.js
@@ -10,3 +10,20 @@ export const blobToBase64 = (files,callback)=>{
 		callback && callback(url)
 	};
 }
+
+
+export const getRgbaColor = ({r,g,b,a})=>{
+	return `rgba(${r},${g},${b},${a})`
+}
+
+export const splitRgbaColor = (color)=>{
+	let rgbaAttr = color.match(/[\d.]+/g);
+	if (rgbaAttr.length >= 3) {
+		var r, g, b, a;
+		r = rgbaAttr[0]
+		g = rgbaAttr[1]
+		b = rgbaAttr[2]
+		a = rgbaAttr[3] || 1
+		return {r,g,b,a}
+	}
+}
